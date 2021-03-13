e7f64b8c553bade8c311dab5176eee1e118201da
colakibrahim01 <63121371+colakibrahim01@users.noreply.github.com>
2021-03-13 14:24:01 -0500

    Update GitDeneme01.java
    2nd Commit from git

--- src/Package1/GitDeneme01.java
+++ src/Package1/GitDeneme01.java
@@ -4,7 +4,7 @@ public class GitDeneme01 {
 
 	public static void main(String[] args) {
 		System.out.println("Git deneme march 12");
-
+		System.out.println("Git deneme from git on March 12");
 	}
 
 }
