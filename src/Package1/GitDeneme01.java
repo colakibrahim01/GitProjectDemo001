ad976d80d257df11c73a601da95669a431e7817d
Ibrahim Colak <colakibrahim01@gmail.com>
2021-03-13 14:27:50 -0500

    3rd

--- src/Package1/GitDeneme01.java
+++ src/Package1/GitDeneme01.java
@@ -5,6 +5,9 @@ public class GitDeneme01 {
 	public static void main(String[] args) {
 		System.out.println("Git deneme march 12");
 		System.out.println("Git deneme from git on March 12");
+		
+		int a = 5;
+		double b = 3.44;
 	}
 
 }
